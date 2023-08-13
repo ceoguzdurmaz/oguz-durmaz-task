16106b3dbba0bc416e2bdc362a7b896e0df4d537
ceoguzdurmaz <116945252+ceoguzdurmaz@users.noreply.github.com>
2023-08-13 22:30:02 +0300

    Made refactoring in store and added new comments.

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { ReactNode } from "react";
 import { useAppDataContext } from "@/context/store";
 import Image from "next/image";
--- src/context/store.tsx
+++ src/context/store.tsx
@@ -1,20 +1,20 @@
-"use client";
+"use client"; // Directive to specify this is client-side code
 
-// External libraries
+// Importing required libraries and utilities
 import React, {
   createContext,
   useContext,
   useState,
   useEffect,
   ReactNode,
-} from "react";
-import axios from "axios";
+} from "react"; // Required React hooks and components
+import axios from "axios"; // Library for making HTTP requests
 
-// Utility and configuration imports
-import { generateSlug, cleanAndLowercaseString } from "@/Utils";
-import { API_URL } from "@/Services";
+// Import utility functions and configurations
+import { generateSlug, cleanAndLowercaseString } from "@/Utils"; // Utility functions
+import { API_URL } from "@/Services"; // API endpoint
 
-// Type imports for data structures and props
+// Type imports for our data structures and component properties
 import {
   TabInfo,
   PluginInfo,
@@ -23,7 +23,7 @@ import {
   APIResponseData,
 } from "@/Types";
 
-// Setting up default values for our context
+// Default data for the context
 const defaultAppData: AppDataProps = {
   tabsData: [],
   pluginsData: [],
@@ -35,24 +35,25 @@ const defaultAppData: AppDataProps = {
   setIsAllPluginsDisabled: () => {},
 };
 
-// Create a new context for the application data
+// Creating a context for the app data
 const AppDataContext = createContext<AppDataProps>(defaultAppData);
 
-// Helper function to assign a slug for each tab based on its title
+// Helper function to format the tab data by adding a slug based on the title
 const formatTabs = (data: Record<string, TabInfo>): TabInfo[] => {
   return Object.values(data).map((tab) => ({
     ...tab,
-    slug: generateSlug(tab.title),
+    slug: generateSlug(tab.title), // Generating a URL-friendly slug
   }));
 };
 
-// Helper function to format the plugins data, categorizing them and assigning properties
+// Helper function to format the plugins' data
 const formatPlugins = (
   formattedTabs: TabInfo[],
   pluginsData: Record<string, PluginInfo>
 ): PluginInfo[] => {
   const formattedPlugins: PluginInfo[] = [];
 
+  // Iterate through each tab and categorize plugins
   for (const tab of formattedTabs) {
     const category = tab.slug;
     const newArr = tab.active.concat(tab.disabled, tab.inactive);
@@ -61,6 +62,7 @@ const formatPlugins = (
     );
     uniqueArr.sort((a, b) => a.localeCompare(b));
 
+    // Iterate through each plugin and assign properties
     uniqueArr.forEach((pluginKey) => {
       const plugin = pluginsData[pluginKey];
       const disabled = tab.disabled.find(
@@ -82,11 +84,11 @@ const formatPlugins = (
   return formattedPlugins;
 };
 
-// This component provides all app related data to its children
+// The component that provides all app-related data to its children
 export const AppDataProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  // State management
+  // Local state management for tabs, plugins, etc.
   const [tabsData, setTabsData] = useState<TabInfo[]>([]);
   const [pluginsData, setPluginsData] = useState<PluginInfo[]>([]);
   const [defaultTab, setDefaultTab] = useState<string>("");
@@ -97,13 +99,19 @@ export const AppDataProvider: React.FC<{ children: ReactNode }> = ({
   const [isAllPluginsDisabled, setIsAllPluginsDisabled] =
     useState<boolean>(false);
 
-  // Function to fetch data from the API and format it
+  // Check if data is available
   const isDataAvailable = pluginsData.length > 0;
+
+  // Function to fetch and format data from the API
   const fetchData = async () => {
     try {
+      // If data isn't loaded, set loading state
       !isDataAvailable && setLoading(true);
 
+      // Make the API call
       const { data } = await axios.get<APIResponseData>(`${API_URL}`);
+
+      // Process the API data
       const formattedTabs = formatTabs(data.tabData);
       const formattedPlugins = formatPlugins(formattedTabs, data.plugins);
       setTabsData(formattedTabs);
@@ -113,13 +121,17 @@ export const AppDataProvider: React.FC<{ children: ReactNode }> = ({
       setIsAllPluginsDisabled(
         formattedPlugins.every((plugin) => plugin.disabled)
       );
+
+      // If data was previously not loaded, reset loading state
       !isDataAvailable && setLoading(false);
     } catch (error) {
-      console.error("Error fetching data:", error);
+      console.error("Error fetching data:", error); // Log errors
     }
   };
-  // Function to toggle the status of a given plugin
+
+  // Function to toggle the active/inactive status of a given plugin
   const togglePluginStatus = async (pluginTitle: string) => {
+    // Find the plugin key by title
     const pluginKey = Object.keys(tempData.data.plugins).find(
       (key) => tempData.data.plugins[key].title === pluginTitle
     );
@@ -129,6 +141,7 @@ export const AppDataProvider: React.FC<{ children: ReactNode }> = ({
       return;
     }
 
+    // Update the tab's active/inactive status based on the plugin
     const updatedTabsData = [...tabsData];
     for (const tab of updatedTabsData) {
       if (tab.active.includes(pluginKey)) {
@@ -142,6 +155,7 @@ export const AppDataProvider: React.FC<{ children: ReactNode }> = ({
       }
     }
 
+    // Update the plugin's status locally
     const updatedPluginsData = pluginsData.map((plugin) => {
       if (plugin.title === pluginTitle) {
         return { ...plugin, status: !plugin.status };
@@ -150,26 +164,32 @@ export const AppDataProvider: React.FC<{ children: ReactNode }> = ({
     });
 
     try {
+      // Send the updated data to the backend
       const data = { ...tempData.data };
       await axios.post(`${API_URL}`, data);
+
+      // Update the local state with the modified data
       setTabsData(updatedTabsData);
       setPluginsData(updatedPluginsData);
     } catch (error) {
       console.error("Error toggling plugin status:", error);
-      // Here you can either revert the changes or notify the user
+      // Handle errors, such as reverting changes or notifying the user
     }
   };
 
   // Function to toggle the disabled/enabled status of all plugins
   const toggleEnableDisableAllPlugins = async () => {
-    const newData = { ...tempData.data };
+    // Create a copy of the temp data
+    const updatedTempData = { ...tempData.data };
+
+    // Check if all plugins are disabled
     const allPluginsAreDisabled = pluginsData.every(
       (plugin) => plugin.disabled
     );
 
-    // Toggle the disabled status in the local data
-    for (const tabKey in newData.tabData) {
-      const tab = newData.tabData[tabKey];
+    // Toggle the disabled status in the temporary data
+    for (const tabKey in updatedTempData.tabData) {
+      const tab = updatedTempData.tabData[tabKey];
       const allPlugins = [...tab.active, ...tab.inactive];
       if (allPluginsAreDisabled) {
         tab.disabled = [];
@@ -180,22 +200,24 @@ export const AppDataProvider: React.FC<{ children: ReactNode }> = ({
 
     try {
       // Send the modified data to the backend
-      await axios.post(`${API_URL}`, newData);
+      await axios.post(`${API_URL}`, updatedTempData);
 
-      // Update the local UI state
-      const updatedPluginsData = pluginsData.map((plugin) => {
-        return { ...plugin, disabled: !allPluginsAreDisabled };
-      });
-      setTabsData(Object.values(newData.tabData)); // Assuming tabData is an object, not an array
-      setPluginsData(updatedPluginsData);
+      // Process the updated data and set it in the local state
+      const formattedTabs = formatTabs(updatedTempData.tabData);
+      const formattedPlugins = formatPlugins(
+        formattedTabs,
+        updatedTempData.plugins
+      );
+      setTabsData(formattedTabs);
+      setPluginsData(formattedPlugins);
       setIsAllPluginsDisabled(!allPluginsAreDisabled);
     } catch (error) {
       console.error("Error toggling disabled plugins:", error);
-      // Handle errors accordingly, e.g., show a notification to the user
+      // Handle errors, such as reverting changes or notifying the user
     }
   };
 
-  // Fetch the data when the component mounts
+  // Fetch data when the component mounts
   useEffect(() => {
     fetchData().then(() => {
       console.log("Data fetched successfully");
@@ -203,6 +225,7 @@ export const AppDataProvider: React.FC<{ children: ReactNode }> = ({
   }, []);
 
   return (
+    // Use context provider to share data with child components
     <AppDataContext.Provider
       value={{
         tabsData,
@@ -220,7 +243,7 @@ export const AppDataProvider: React.FC<{ children: ReactNode }> = ({
   );
 };
 
-// Custom hook to make it easier to use our App data context
+// Custom hook for accessing our App data context
 export const useAppDataContext = (): AppDataProps => {
   const context = useContext(AppDataContext);
   if (!context) {
